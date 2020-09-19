1286ca55173c312bfcbe8095c996a8b19777ab0e
huawen-poppy <70507602+huawen-poppy@users.noreply.github.com>
2020-09-19 16:40:56 +0800

    Update 1.8Approximate_Pattern_Match.py

--- 1.8Approximate_Pattern_Match.py
+++ 1.8Approximate_Pattern_Match.py
@@ -2,7 +2,7 @@ def Approximate_Pattern_Match(Pattern,Text,d):
     '''return the positions of approximate pattern in text with at most d mismatches'''
     L=len(Pattern)
     approximate_pattern_first_position=[]
-    for i in range(len(Text) - L):
+    for i in range(len(Text) - L+1):
         compare_string=Text[i:i+L]
         distance=0
         for j in range(len(Pattern)):
