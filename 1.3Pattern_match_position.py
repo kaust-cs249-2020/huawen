8f1e7d4966bdd215e6fa484f6b02a107fd7cd61f
huawen-poppy <70507602+huawen-poppy@users.noreply.github.com>
2020-09-19 16:39:32 +0800

    Update 1.3Pattern_match_position.py

--- 1.3Pattern_match_position.py
+++ 1.3Pattern_match_position.py
@@ -1,7 +1,7 @@
 def Pattern_match_position(Text,Pattern):
     '''find out the pattern's location on genes'''
     position=[]
-    for i in range(len(Text)-len(Pattern)):
+    for i in range(len(Text)-len(Pattern)+1):
         if Text[i:i+len(Pattern)] == Pattern:
             position.append(i)
     for item in position:
