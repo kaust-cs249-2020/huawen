43367b66319e02d0933b0d50b177021e9b849000
huawen-poppy <70507602+huawen-poppy@users.noreply.github.com>
2020-09-19 16:37:26 +0800

    Update 1.2BetterFrequentWords.py

--- 1.2BetterFrequentWords.py
+++ 1.2BetterFrequentWords.py
@@ -2,7 +2,7 @@
 def FrequencyTable(Text,k):
     freqMap={}
     n=len(Text)
-    for i in range(n-k):
+    for i in range(n-k+1):
         Pattern=Text[i:i+k]
         if Pattern not in freqMap.keys():
             freqMap[Pattern]=1
